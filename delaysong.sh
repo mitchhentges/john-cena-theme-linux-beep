9cf53359443a1569fd734ccbb932bd5d1f1ebab6
Mitchell Hentges <mitch9654@gmail.com>
2014-11-14 16:17:52 -0800

    Two stanzas are go

---
+++ delaysong.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 3
+bash startupsong.sh
+exit 0
--- startupsong.sh
+++ startupsong.sh
@@ -1,7 +1,15 @@
-beep -f 19999 -l 375
+#!/bin/bash
+
+beep -f 659 -l 375
 beep -f 1175 -l 375 
 beep -f 1319 -l 188 
-beep -f 1047 -l 188 
+beep -f 1047 -l 377 
 beep -f 1175 -l 1250 
 
+beep -f 659 -l 375 
+beep -f 1397 -l 375
+beep -f 1319 -l 188
+beep -f 1047 -l 377
+beep -f 1175 -l 1250
 
+exit 0
